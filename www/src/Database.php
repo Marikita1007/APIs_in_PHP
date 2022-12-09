44ca3d6ffd4f25853da37134b425062d0d4c5913
Marikita1007 <ishinomaki1007@gmail.com>
2022-12-09 17:08:01 +0100

    35. Configure PDO to prevent numeric values from being converted to strings

--- www/src/Database.php
+++ www/src/Database.php
@@ -16,7 +16,11 @@ class Database
         $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
 
         return new PDO($dsn, $this->user, $this->password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            //Add down below because Json returns data as a string regardless of its data type.
+            //By addinf them integer becomes numbers.
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_STRINGIFY_FETCHES => false
         ]);
     }
 }
