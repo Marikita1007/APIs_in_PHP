1c138d9f4cd3dcd71f7732d7a8509d5495e129cf
Marikita1007 <ishinomaki1007@gmail.com>
2022-12-15 14:44:32 +0100

    58. Cache the database connection to avoid multiple connections in the same request

--- www/src/Database.php
+++ www/src/Database.php
@@ -2,6 +2,9 @@
 
 class Database
 {
+    //By adding ? before PDO statement, it shows that it's a nullable. 
+    private ?PDO $conn = null;
+
     public function __construct(
         private string $host,
         private string $name,
@@ -12,15 +15,19 @@ class Database
 
     public function getConnection(): PDO
     {
-        //$dsn stand for data source name 
-        $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
+        if($this->conn === null){
+            //$dsn stand for data source name 
+            $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
 
-        return new PDO($dsn, $this->user, $this->password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            //Add down below because Json returns data as a string regardless of its data type.
-            //By adding them integer becomes numbers. データベース内の数値は、JSONとして返すときに文字列に変換されない。
-            PDO::ATTR_EMULATE_PREPARES => false,
-            PDO::ATTR_STRINGIFY_FETCHES => false,
-        ]);
+            $this->conn = new PDO($dsn, $this->user, $this->password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                //Add down below because Json returns data as a string regardless of its data type.
+                //By adding them integer becomes numbers. データベース内の数値は、JSONとして返すときに文字列に変換されない。
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::ATTR_STRINGIFY_FETCHES => false,
+            ]);   
+        }
+
+        return $this->conn;
     }
 }
