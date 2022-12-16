a8dc91066e3aa40f0cf452f6e2303ba982ce836a
Marikita1007 <ishinomaki1007@gmail.com>
2022-12-16 17:22:40 +0100

    76. Add an expiry claim to the access token payload when logging in

--- www/api/login.php
+++ www/api/login.php
@@ -52,8 +52,9 @@ $payload = [
     //In a JWT, the indexes for the items in the payload need to be specific values.
     //JWTでは、ペイロードのアイテムのインデックス("id","name")が特定の値である必要があり。url:https://www.iana.org/assignments/jwt/jwt.xhtml
     "sub" => $user["id"],//For the "id", we use "sub" as subject claim.
-    "name" => $user["name"]
+    "name" => $user["name"],
     //JWT(JSON Web Token Claims)の名前規定に従うことで、ペイロードは、認証に成功したときにJWTアクセストークンにエンコードされるユーザーに関するデータにエンコードされる。
+    "exp" => time() + 20 //76. Add an expiry claim to the access token payload when logging in
 ];
 
 //このアクセストークンは、ユーザーのIDと名前をJONでエンコード(0、1の組み合わせ（コード）と文字の対応表』を別の対応表に切り替えること」)し、次にBase64でエンコードする単純な文字列。このトークンを検証するときは、このプロセスを逆にして解読するだけ。これは簡単に偽造可能。base64_encodeでなく、安全なアクセストークンを作成する方法が必要。そこで、業界標準の方法、JSON Web Tokens もしくは JWTsを使用する。
