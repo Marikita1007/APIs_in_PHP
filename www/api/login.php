2be1f01d0d96811cbedfdf5377f148b92f7f41d5
Marikita1007 <ishinomaki1007@gmail.com>
2022-12-16 16:21:53 +0100

    69. Generate a JWT access token in the login endpoint containing JWT claims

--- www/api/login.php
+++ www/api/login.php
@@ -49,12 +49,19 @@ if (!password_verify($data["password"], $user["password_hash"])){
 //63. Generate an encoded access token containing the user details
 //Remember the idea with an access token is that we can authorise a request to the API without doing
 $payload = [
-    "id" => $user["id"],
+    //In a JWT, the indexes for the items in the payload need to be specific values.
+    //JWTでは、ペイロードのアイテムのインデックス("id","name")が特定の値である必要があり。url:https://www.iana.org/assignments/jwt/jwt.xhtml
+    "sub" => $user["id"],//For the "id", we use "sub" as subject claim.
     "name" => $user["name"]
+    //JWT(JSON Web Token Claims)の名前規定に従うことで、ペイロードは、認証に成功したときにJWTアクセストークンにエンコードされるユーザーに関するデータにエンコードされる。
 ];
 
 //このアクセストークンは、ユーザーのIDと名前をJONでエンコード(0、1の組み合わせ（コード）と文字の対応表』を別の対応表に切り替えること」)し、次にBase64でエンコードする単純な文字列。このトークンを検証するときは、このプロセスを逆にして解読するだけ。これは簡単に偽造可能。base64_encodeでなく、安全なアクセストークンを作成する方法が必要。そこで、業界標準の方法、JSON Web Tokens もしくは JWTsを使用する。
-$access_token = base64_encode(json_encode($payload));
+//$access_token = base64_encode(json_encode($payload));
+
+//69. Generate a JWT access token in the login endpoint containing JWT claims
+$codec = new JWTCodec;
+$access_token = $codec->encode($payload);
 
 echo json_encode([
     "access_token" => $access_token
